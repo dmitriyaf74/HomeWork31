b91058e4b73e21d0e8d80855e4719f403f46f4d3
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-04-20 18:59:28 +0500

    fix: 2

--- Librarian/Classes/Library.cs
+++ Librarian/Classes/Library.cs
@@ -14,14 +14,12 @@ namespace Librarian.Classes
     internal class Library
     {
         private MyCD _data = new MyCD();
-        private BlockingCollection<KeyValuePair<string, int>> _queue = new BlockingCollection<KeyValuePair<string, int>>();
         private Task _workerTask;
         private CancellationTokenSource _cts = new CancellationTokenSource();
 
 
         public Library()
         {
-            // Запускаем задачу-обработчик, которая будет брать элементы из очереди.
             _workerTask = Task.Run(() => ProcessQueue(_cts.Token));
             _workerTask.ConfigureAwait(false);
         }
@@ -32,7 +30,6 @@ namespace Librarian.Classes
         public void AddBook(string BookName) 
         {
             _data.TryAdd(BookName, 0);
-            _queue.Add(new KeyValuePair<string, int>(BookName, 0));
         }
 
         private void ProcessQueue(CancellationToken token)
@@ -45,31 +42,23 @@ namespace Librarian.Classes
                     break;
 
                 Random random = new Random();
-                int randomNumber = random.Next(0, _data.Count);
-                var key = _data.ElementAt(randomNumber).Key;
+                int randomNumber = random.Next(0, 999);
+                var key = _data.ElementAt(randomNumber % _data.Count).Key;
                 _data[key]++;
                 if (_data[key] == 100)
                 {
                     int value;
                     _data.Remove(key, out value);
                 }
-                Thread.Sleep(100);
-
-                //for (int i = 0; i < _data.Count; i++)
-                //    _data[_data.ElementAt(i).Key]++;
+                Thread.Sleep(1000);
             }
         }
 
         public void Stop()
         {
-            // Сигнализируем об отмене задачи-обработчика.
             _cts.Cancel();
-            // Завершаем добавление новых элементов в очередь.
-            _queue.CompleteAdding();
-            // Ожидаем завершения задачи-обработчика (с таймаутом).
             _workerTask.Wait(5000);
             _cts.Dispose();
-            _queue.Dispose();
         }
 
     }
