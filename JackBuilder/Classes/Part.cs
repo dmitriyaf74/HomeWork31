be3420e2812fdbbac7aab6ad4a32d9f36c931f31
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-04-21 10:00:12 +0500

    fix: Jack Builder -> List to ImmutableList

--- JackBuilder/Classes/Part.cs
+++ JackBuilder/Classes/Part.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace JackBuilder.Classes
     internal abstract class Part
     {
 
-        protected int PrevPartRow(List<string> APoem)
+        protected int PrevPartRow(ImmutableList<string> APoem)
         {
             for (int i = APoem.Count - 1; i >= 0; i--)
                 if (APoem[i] == "")
@@ -17,23 +18,23 @@ namespace JackBuilder.Classes
             return -1;
         }
 
-        public abstract Part AddPart(List<string> APoem);
-        public abstract List<string> GetPoem();
-        public List<string> Poem { get { return GetPoem(); } }
-        protected Part AddCustomPart(List<string> APoem, List<string> A_poem)
+        public abstract Part AddPart(ImmutableList<string> APoem);
+        public abstract ImmutableList<string> GetPoem();
+        public ImmutableList<string> Poem { get { return GetPoem(); } }
+        protected Part AddCustomPart(ImmutableList<string> APrevPoem, ref ImmutableList<string> A_poem)
         {
             var startCount = A_poem.Count;
 
-            for (int i = 0; i < APoem.Count; i++)
+            for (int i = 0; i < APrevPoem.Count; i++)
             {
-                A_poem.Insert(i, APoem[i]);
+                A_poem = A_poem.Insert(i, APrevPoem[i]);
             }
 
-            A_poem.Insert(APoem.Count, "");
+            A_poem = A_poem.Insert(APrevPoem.Count, "");
 
-            for (int i = PrevPartRow(APoem) + startCount; i < APoem.Count; i++)
+            for (int i = PrevPartRow(APrevPoem) + startCount; i < APrevPoem.Count; i++)
             {
-                A_poem.Insert(A_poem.Count, APoem[i]);
+                A_poem = A_poem.Insert(A_poem.Count, APrevPoem[i]);
             }
             return this;
         }
@@ -42,75 +43,75 @@ namespace JackBuilder.Classes
 
     internal class Part1 : Part
     {
-        public Part1(string[] Poem) { _poem.AddRange(Poem); }
+        public Part1(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part2 : Part
     {
-        public Part2(string[] Poem) { _poem.AddRange(Poem); }
+        public Part2(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part3 : Part
     {
-        public Part3(string[] Poem) { _poem.AddRange(Poem); }
+        public Part3(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part4 : Part
     {
-        public Part4(string[] Poem) { _poem.AddRange(Poem); }
+        public Part4(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part5 : Part
     {
-        public Part5(string[] Poem) { _poem.AddRange(Poem); }
+        public Part5(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part6 : Part
     {
-        public Part6(string[] Poem) { _poem.AddRange(Poem); }
+        public Part6(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part7 : Part
     {
-        public Part7(string[] Poem) { _poem.AddRange(Poem); }
+        public Part7(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
 
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part8 : Part
     {
-        public Part8(string[] Poem) { _poem.AddRange(Poem); }
+        public Part8(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
     internal class Part9 : Part
     {
-        public Part9(string[] Poem) { _poem.AddRange(Poem); }
+        public Part9(string[] Poem) { _poem = _poem.AddRange(Poem); }
 
-        private List<string> _poem = new List<string>();
-        public override List<string> GetPoem() { return _poem; }
-        public override Part AddPart(List<string> APoem) { return AddCustomPart(APoem, _poem); }
+        private ImmutableList<string> _poem = ImmutableList<string>.Empty;
+        public override ImmutableList<string> GetPoem() { return _poem; }
+        public override Part AddPart(ImmutableList<string> APoem) { return AddCustomPart(APoem, ref _poem); }
     }
 }
